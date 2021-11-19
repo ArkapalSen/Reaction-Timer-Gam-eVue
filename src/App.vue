4cce816dd1ee2e64583a2e4626fea0cee3600093
ArkapalSen <arkapalsen@gmail.com>
2021-11-19 20:31:13 +0530

    --19nov21

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,41 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <h1>Reaction Timer..Be your own Champ</h1>
+  <button @click='start' :disabled='isPlaying'>play</button>
+  <Block v-if='isPlaying' v-bind:delay='delay' @end='endGame'/>
+  <Results v-if='showResults' :result='score'/>
 </template>
+
+<script>
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
+
+  export default{
+    name : 'App',
+    components : {
+      Block,Results,
+    },
+    data(){
+      return{
+        isPlaying : false,                               //true if playing..false if not playing (at start too)
+        delay: null,
+        score:null,
+        showResults : false,
+      }
+    },
+    methods:{
+      start(){
+        this.delay = 2000 + Math.random() * 5000,       //bw 2s-7s any random time
+        this.isPlaying = true
+        this.showResults = false
+      },
+      endGame(reactTime){
+        this.score = reactTime
+        this.showResults = true
+        this.isPlaying = false
+      },
+    }
+  }
+</script>
 
 <style>
 #app {
@@ -13,18 +44,22 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
+}
+button{
+  background: #0faf87;
+  color : white;
+  border : none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin : 10px;
+}
+button[disabled]{
+  capacity : 0.3;
+  cursor : not-allowed;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>Your score : {{result}}ms</h1>
+    <h2 class="rank">You are like {{resultMsg}} </h2>
+</template>
+
+<script>
+    export default{
+        name : 'Results',
+        props : ['result'],
+        data(){
+            return{
+                resultMsg : '',
+            }
+        },
+        mounted() {
+            this.resultMsg = this.result < 350 ? 'Hare 🐇 You Win 😎' : 'Tortoise 🐢 You Loose 💔'
+        },
+    }
+</script>
+
+<style scoped>
+    .rank{
+        font-size: 1.4rem;;
+        color : #0faf87;
+        font-weight: bold;
+    }
+</style>
